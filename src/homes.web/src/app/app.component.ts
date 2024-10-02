818befeb1be18ce672fe182752e39cd2d2456356
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-10-02 16:58:14 +0100

    Completed homes project

--- src/homes.web/src/app/app.component.ts
+++ src/homes.web/src/app/app.component.ts
@@ -1,13 +1,17 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [DatePipe, HomeComponent, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'homes.web';
+  title = 'homes';
+  date = new Date();
 }
